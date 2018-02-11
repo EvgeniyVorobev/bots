d79062b28d9df48db83f0308e84b386f70335930
Evgeniy Vorobev <evggordadym@gmail.com>
2018-02-12 00:33:21 +0200

    test bot

--- index.js
+++ index.js
@@ -21,10 +21,6 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/test_evg', function(request, response) {
-  response.send(cool());
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
