02aab9d0206ace72fe9a4951458bc348515b57e2
Evgeniy Vorobev <evggordadym@gmail.com>
2018-02-12 00:30:27 +0200

    test bot

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ bot.on('message', msg => {
 		bot.sendMessage(msg.chat.id,`Привет, тебя звать-то ${msg.from.first_name} ?:)`);
 })
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
+views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-// app.get('/', function(request, response) {
-//   response.render('pages/index')
-// });
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
 
-// app.get('/test_evg', function(request, response) {
-//   response.send(cool());
-// });
+app.get('/test_evg', function(request, response) {
+  response.send(cool());
+});
 
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
 
