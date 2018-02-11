9d3debbddba00a1c978297dc34444652b001fdf1
Evgeniy Vorobev <evggordadym@gmail.com>
2018-02-12 00:56:21 +0200

    test bot

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var app = express();
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '504046177:AAE1RcGBnSqhaODmg9Pj5aQlzeW6s3gDXf4';
 const bot = new TelegramBot(TOKEN, {polling: true});
-
+app.set('port', (process.env.PORT || 5000));
 bot.on('message', msg => {
 		bot.sendMessage(msg.chat.id,`Привет, тебя звать-то ${msg.from.first_name} ?:)`);
 })
 
-app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+
+// app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 // app.get('/', function(request, response) {
 //   response.render('pages/index')
