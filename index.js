116d83f02c2f593b12736909361d4fa5b4beb72f
Evgeniy Vorobev <evggordadym@gmail.com>
2018-02-12 01:01:24 +0200

    test bot

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var app = express();
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '504046177:AAE1RcGBnSqhaODmg9Pj5aQlzeW6s3gDXf4';
 const bot = new TelegramBot(TOKEN, {polling: true});
-app.set('port', (process.env.PORT));
+var port = process.env.PORT || 5000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 5000");
+});
 bot.on('message', msg => {
 		bot.sendMessage(msg.chat.id,`Привет, тебя звать-то ${msg.from.first_name} ?:)`);
 })
