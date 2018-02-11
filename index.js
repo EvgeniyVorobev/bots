b6cd24454d18037a436d076d47e97cca127a009e
Evgeniy Vorobev <evggordadym@gmail.com>
2018-02-12 00:42:09 +0200

    test bot

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express();
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '504046177:AAE1RcGBnSqhaODmg9Pj5aQlzeW6s3gDXf4';
-
 const bot = new TelegramBot(TOKEN, {polling: true});
+
 bot.on('message', msg => {
 		bot.sendMessage(msg.chat.id,`Привет, тебя звать-то ${msg.from.first_name} ?:)`);
 })
